c67653e7bc9732b162b4e3c2197218ab6ebf7deb
Facu Murga <fmurga2011@gmail.com>
2023-02-13 19:58:24 -0300

    minor js fixes, scroll on link click

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
-document.addEventListener( 'DOMContentLoaded', function() {
-    const splide = new Splide( '.splide',  {
-        rewind     : true,
+
+const setActiveClass = (event) => {
+    const links = document.querySelectorAll(".nav-item-link");
+    links.forEach((link) => {
+        link.classList.remove("active");
+    });
+    event.currentTarget.classList.add("active");
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    const splide = new Splide('.splide', {
+        rewind: true,
         rewindSpeed: 1000,
         rewindByDrag: true,
         autoplay: true,
@@ -19,19 +28,29 @@ document.addEventListener( 'DOMContentLoaded', function() {
         start: 1,
         arrows: false,
         loop: true,
-      } );
+    });
     splide.mount();
 
     const links = document.querySelectorAll(".nav-item-link");
     links.forEach((link) => {
-        link.addEventListener("click", setActiveClass);
+        link.addEventListener("click", (e) => {
+            e.preventDefault()
+            setActiveClass(e);
+        });
     });
-  } );
 
-  const setActiveClass = (event) => {
-    const links = document.querySelectorAll(".nav-item-link");
-    links.forEach((link) => {
-      link.classList.remove("active");
+    const container = document.getElementById("navigation");
+
+    container.addEventListener("click", (event) => {
+      if (event.target.classList.contains("nav-item-link")) {
+        event.preventDefault();
+        const originalUrl = event.target.href;
+        const fragmentIdentifier = originalUrl.split('#')[1];
+        const section = document.getElementById(fragmentIdentifier);
+    
+        if (section) {
+          section.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
     });
-    event.currentTarget.classList.add("active");
-  };
+});
